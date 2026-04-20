a4a37cf38aa4ee3f82f96b98444e081b14b750ae
Zeen <zeenabdulal2023@gmail.com>
2026-04-20 20:42:28 +0530

    Add Spring Boot source code as regular files

---
+++ bikeRentalAndRideSharing/src/main/java/com/projectGroupWD55/bikeRentalAndRideSharing/BikeRentalAndRideSharingApplication.java
@@ -0,0 +1,13 @@
+package com.projectGroupWD55.bikeRentalAndRideSharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRentalAndRideSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeRentalAndRideSharingApplication.class, args);
+	}
+
+}
