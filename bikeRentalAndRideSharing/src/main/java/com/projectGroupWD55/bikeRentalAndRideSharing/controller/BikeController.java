403072e08de67f989a647b822aaceee88f20e2e9
Zeen <zeenabdulal2023@gmail.com>
2026-05-15 14:32:55 +0530

    made index and maincontroller

--- bikeRentalAndRideSharing/src/main/java/com/projectGroupWD55/bikeRentalAndRideSharing/controller/BikeController.java
+++ bikeRentalAndRideSharing/src/main/java/com/projectGroupWD55/bikeRentalAndRideSharing/controller/BikeController.java
@@ -1,7 +1,6 @@
 package com.projectGroupWD55.bikeRentalAndRideSharing.controller;
 import com.projectGroupWD55.bikeRentalAndRideSharing.dto.BikeRequestDTO;
 import com.projectGroupWD55.bikeRentalAndRideSharing.dto.BikeResponseDTO;
-import com.projectGroupWD55.bikeRentalAndRideSharing.entity.Bike;
 import com.projectGroupWD55.bikeRentalAndRideSharing.service.BikeService;
 import org.springframework.web.bind.annotation.*;
 
